f18ea455b6c196ab54c9fdfa1305addaae30d34c
Patrick Wilker Vieira Mota <patrickvieiramota@gmail.com>
2020-03-27 14:56:07 -0300

    Back-end: testes

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const { errors } = require('celebrate');
 const routes = require('./routes');//para entender que routes é um arquivo e nao um pacote como o express
 
 const app = express();
@@ -7,5 +8,8 @@ const app = express();
 app.use(cors()); // vai permitir que o front-end tenha acesso. dentro do cors() da para por {origin: 'http://nomedosite.com'} quando for hospeado
 app.use(express.json());
 app.use(routes);
+app.use(errors()); //importante abrir e fechar os parenteses
 
-app.listen(3333);
+//app.listen(3333);
+
+module.exports = app;
